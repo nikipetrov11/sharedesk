83f068afd1166368ecd39fda21306c66ce089c5d
nikipetrov11 <60786622+nikipetrov11@users.noreply.github.com>
2023-06-05 23:31:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,41 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server);
-
-// Serve the static HTML file
-app.use(express.static('public'));
-
-// Store room data
-const rooms = new Map();
+const io = socketIO(server);
 
 // Handle socket connections
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('A user connected');
 
-  // Handle joining a room
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).emit('user-connected', userId);
-
-    // Store user in the room
-    if (!rooms.has(roomId)) {
-      rooms.set(roomId, new Map());
-    }
-    rooms.get(roomId).set(socket.id, userId);
-
-    // Send existing users in the room to the newly joined user
-    const existingUsers = Array.from(rooms.get(roomId).values());
-    socket.emit('existing-users', existingUsers);
+  // Event handler for the "offer" message
+  socket.on('offer', offer => {
+    // Broadcast the offer to all other connected clients
+    socket.broadcast.emit('offer', offer);
   });
 
-  // Handle signaling messages
-  socket.on('message', (roomId, userId, message) => {
-    socket.to(roomId).emit('message', userId, message);
+  // Event handler for the "answer" message
+  socket.on('answer', answer => {
+    // Broadcast the answer to all other connected clients
+    socket.broadcast.emit('answer', answer);
   });
 
-  // Handle disconnections
+  // Event handler for the "iceCandidate" message
+  socket.on('iceCandidate', candidate => {
+    // Broadcast the ICE candidate to all other connected clients
+    socket.broadcast.emit('iceCandidate', candidate);
+  });
+
+  // Event handler for disconnections
   socket.on('disconnect', () => {
     console.log('A user disconnected');
-    const roomId = getRoomId(socket);
-    if (roomId) {
-      socket.to(roomId).emit('user-disconnected', rooms.get(roomId).get(socket.id));
-      rooms.get(roomId).delete(socket.id);
-      if (rooms.get(roomId).size === 0) {
-        rooms.delete(roomId);
-      }
-    }
   });
 });
 
-// Get the room ID for a socket
-function getRoomId(socket) {
-  const room = Array.from(socket.rooms.values()).find(roomId => roomId !== socket.id);
-  return room || null;
-}
-
 // Start the server
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Signaling server is running on port ${port}`);
 });
